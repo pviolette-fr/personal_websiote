9c150ede1b7aca4d675a6672f81cfddf00040f9b
pviolette-fr <32514197+pviolette-fr@users.noreply.github.com>
2018-06-09 23:33:54 +0200

    Site template

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+          integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <style>
+        @yield('style')
+    </style>
+</head>
+<body>
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="{{route('home')}}">
+            <span class="icon">
+              <i class="fas fa-home"></i>
+            </span>
+        </a>
+    </div>
+    <div class="navbar-menu">
+        <div class="navbar-start">
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    About me
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Curriculum Vitae
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    University Projects
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        SnakeRPG
+                    </a>
+                    <a class="navbar-item">
+                        LIDE
+                    </a>
+                </div>
+            </div>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Personal Projects
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Placeholder
+                    </a>
+                    <a class="navbar-item">
+                        Placeholder
+                    </a>
+                    <a class="navbar-item">
+                        Placeholder
+                    </a>
+                    <a class="navbar-item">
+                        Placeholder
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+        </div>
+    </div>
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+    </a>
+</nav>
+@yield('content')
+</body>
+</html>
