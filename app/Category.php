806b016a9b1b3b1e9885cccd47e9225368fc2767
pviolette-fr <32514197+pviolette-fr@users.noreply.github.com>
2018-06-10 10:06:35 +0200

    Define relationship in model

--- app/Category.php
+++ app/Category.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $primaryKey = 'id';
-    //
+
+    /**
+     * Define the One-to-Many Category/Project relationship.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function projects()
+    {
+        return $this->hasMany('App\Project');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Category', 'parent_id')
+            ->withDefault([
+                'name' => 'Root',
+                'description' => 'Root category',
+                'published' => true
+            ]);
+    }
+
+    public function children()
+    {
+        return $this->hasMany('App\Category', 'parent_id');
+    }
 }
--- app/Project.php
+++ app/Project.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
 
+    /**
+     * Define the One-to-Many (inverse) Category/Project relationship
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo("App\Category");
+    }
+
 }
